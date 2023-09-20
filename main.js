e4cc2a30904127a2c9d915aeb8c6a88f2c94e868
JrSchoolAccount <jesperx2000@gmail.com>
2023-09-20 16:07:41 +0200

    changed name

--- main.js
+++
@@ -1,6 +0,0 @@
-const myBtn = document.getElementById("btn");
-const myText = document.getElementById("text")
-
-myBtn.addEventListener("click", () => {
-    myText.innerHTML = "Goodbye World";
-});
